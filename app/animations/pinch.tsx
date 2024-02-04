4b26277a3034b4d8348877ec133dd6b3d186ce2a
luccagugescu <luccagugescu@gmail.com>
2024-02-04 17:38:19 +0100

    add pinch and pan for image

--- app/animations/pinch.tsx
+++ app/animations/pinch.tsx
@@ -4,29 +4,58 @@ import {
 	Gesture,
 	GestureDetector,
 	GestureHandlerRootView,
-} from 'react-native-gesture-handler'; import Animated, { useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
+} from 'react-native-gesture-handler'; import Animated, { runOnJS, useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
 import { StyleSheet } from "nativewind";
+const END_POSITION = 200;
 
 export default function Pan() {
-	const scale = useSharedValue(1);
-	const savedScale = useSharedValue(1);
+	const scale = useSharedValue(1)
+	const translateX = useSharedValue(0)
+	const translateY = useSharedValue(0)
 
 	const pinchGesture = Gesture.Pinch()
+
 		.onUpdate((e) => {
-			scale.value = savedScale.value * e.scale;
+			scale.value = e.scale
 		})
 		.onEnd(() => {
-			savedScale.value = scale.value;
-		});
+			scale.value = withTiming(1)
+		})
 
-	const animatedStyle = useAnimatedStyle(() => ({
-		transform: [{ scale: scale.value }],
-	}));
+	const panGesture = Gesture.Pan()
+	panGesture.enableTrackpadTwoFingerGesture(true)
+	panGesture.minPointers(2)
 
+	panGesture
+		.onUpdate(e => {
+			'worklet'
+			translateX.value = e.translationX
+			translateY.value = e.translationY
+		})
+
+	panGesture
+		.onEnd(e => {
+			'worklet'
+			translateX.value = withTiming(0)
+			translateY.value = withTiming(0)
+		})
+
+	const composed = Gesture.Simultaneous(pinchGesture, panGesture)
+
+	const animatedStyle = useAnimatedStyle(() => {
+		return {
+			zIndex: 50,
+			transform: [
+				{ scale: scale.value },
+				{ translateX: translateX.value },
+				{ translateY: translateY.value },
+			],
+		}
+	})
 	return (
 		<GestureHandlerRootView className="flex-1">
 			<View className="flex flex-row items-center justify-center h-full">
-				<GestureDetector gesture={pinchGesture}>
+				<GestureDetector gesture={composed}>
 					<Animated.View style={[styles.box, animatedStyle]}>
 						<Image style={[StyleSheet.absoluteFill, styles.box]} source={{ uri: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.1448711260.1706918400&semt=sph" }} />
 					</Animated.View>
