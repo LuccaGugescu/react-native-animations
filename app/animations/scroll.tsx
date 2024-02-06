aef28d4a015e78ea2379c678da4db7ff8f113e46
luccagugescu <luccagugescu@gmail.com>
2024-02-06 08:38:26 +0100

    use variables for spacing

--- app/animations/scroll.tsx
+++ app/animations/scroll.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import { StatusBar, FlatList, Image, Text, View, Dimensions, StyleSheet, TouchableOpacity, Easing, SafeAreaViewBase, SafeAreaView } from 'react-native';
+import { StatusBar, FlatList, Image, Text, View, Dimensions, StyleSheet, TouchableOpacity, Easing, SafeAreaViewBase, SafeAreaView, Animated } from 'react-native';
 import { faker } from '@faker-js/faker';
-import Animated, { Extrapolation, interpolate, useAnimatedRef, useAnimatedScrollHandler, useAnimatedStyle, useScrollViewOffset, useSharedValue } from 'react-native-reanimated';
 
 const { width, height } = Dimensions.get('screen');
 faker.seed(10);
@@ -19,35 +18,24 @@ const SPACING = 20;
 const AVATAR_SIZE = 70;
 
 export default function Scroll() {
-    const aref = useAnimatedRef<Animated.ScrollView>();
-    const scrollHandler = useScrollViewOffset(aref);
-
-    useAnimatedStyle(() => {
-        console.log(scrollHandler.value);
-        return {
-
-        };
-    });
-
-
 
     return (
         <View style={{ flex: 1, backgroundColor: '#fff' }}>
             <Image blurRadius={5} source={{ uri: "https://plus.unsplash.com/premium_photo-1706625695154-d8a6a1c275d8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }} style={StyleSheet.absoluteFillObject} />
             <StatusBar hidden />
             <Animated.FlatList
-                ref={aref}
+                contentContainerStyle={{ padding: SPACING }}
+                scrollEventThrottle={16}
                 data={DATA}
                 keyExtractor={item => item.key}
                 renderItem={item => {
                     return (
-                        <Animated.View style={{ transform: [{ scale: scrollHandler }] }} className='flex flex-row items-center m-4 p-4 bg-gray-200 rounded-2xl shadow-lg shadow-black'>
-                            <Image src={item.item.image} style={{ width: AVATAR_SIZE, height: AVATAR_SIZE, borderRadius: AVATAR_SIZE }} />
+                        <Animated.View className='flex flex-row bg-gray-200 shadow-xl shadow-black rounded-2xl' style={{ padding: SPACING, marginBottom: SPACING }}>
+                            <Image src={item.item.image} style={{ width: AVATAR_SIZE, height: AVATAR_SIZE, borderRadius: AVATAR_SIZE, marginRight: SPACING / 2 }} />
                             <View style={{ marginLeft: SPACING }}>
                                 <Text className='font-bold text-lg'>{item.item.name}</Text>
                                 <Text className='text-blue-300'>{item.item.email}</Text>
                                 <Text>{item.item.jobTitle}</Text>
-
                             </View>
                         </Animated.View>)
                 }
