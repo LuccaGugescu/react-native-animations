690413f6fed94d624011564dd0a1a8249352c551
luccagugescu <luccagugescu@gmail.com>
2024-02-06 14:22:17 +0100

    migrate to reanimated

--- app/animations/scroll.tsx
+++ app/animations/scroll.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { StatusBar, FlatList, Image, Text, View, Dimensions, StyleSheet, TouchableOpacity, Easing, SafeAreaViewBase, SafeAreaView, Animated } from 'react-native';
 import { faker } from '@faker-js/faker';
+import Animated, { SharedValue, interpolate, useAnimatedScrollHandler, useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
-const { width, height } = Dimensions.get('screen');
 faker.seed(10);
 const DATA = [...Array(30).keys()].map((_, i) => {
     return {
@@ -19,57 +20,70 @@ const AVATAR_SIZE = 70;
 const ITEM_SIZE = AVATAR_SIZE + SPACING * 3;
 
 export default function Scroll() {
-    const scrollY = React.useRef(new Animated.Value(0)).current;
+    const scrollY = useSharedValue(0);
+    const handleScroll = useAnimatedScrollHandler(event => {
+        scrollY.value = event.contentOffset.y;
+    });
     return (
         <View style={{ flex: 1, backgroundColor: '#fff' }}>
             <Image blurRadius={5} source={{ uri: "https://plus.unsplash.com/premium_photo-1706625695154-d8a6a1c275d8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }} style={StyleSheet.absoluteFillObject} />
             <StatusBar hidden />
             <Animated.FlatList
-                onScroll={Animated.event([{ nativeEvent: { contentOffset: { y: scrollY } } }], { useNativeDriver: true })}
+                onScroll={handleScroll}
                 contentContainerStyle={{ padding: SPACING }}
                 scrollEventThrottle={16}
                 data={DATA}
                 keyExtractor={item => item.key}
-                renderItem={item => {
-                    const inputRange = [
-                        -1,
-                        0,
-                        ITEM_SIZE * item.index,
-                        ITEM_SIZE * (item.index + 2)
-                    ]
+                renderItem={item =>
 
-                    const inputRangeOpacity = [
-                        -1,
-                        0,
-                        ITEM_SIZE * item.index,
-                        ITEM_SIZE * (item.index + .5)
-                    ]
 
-                    const opacity = scrollY.interpolate(
-                        {
-                            inputRange: inputRangeOpacity,
-                            outputRange: [1, 1, 1, 0]
-                        }
-                    )
-                    const scale = scrollY.interpolate(
-                        {
-                            inputRange: inputRange,
-                            outputRange: [1, 1, 1, 0]
-                        }
-                    )
-                    return (
-                        <Animated.View className='flex flex-row bg-gray-200 shadow-xl shadow-black rounded-2xl' style={{ padding: SPACING, marginBottom: SPACING, transform: [{ scale: scale }], opacity: opacity }}>
-                            <Image src={item.item.image} style={{ width: AVATAR_SIZE, height: AVATAR_SIZE, borderRadius: AVATAR_SIZE, marginRight: SPACING / 2 }} />
-                            <View style={{ marginLeft: SPACING }}>
-                                <Text className='font-bold text-lg'>{item.item.name}</Text>
-                                <Text className='text-blue-300'>{item.item.email}</Text>
-                                <Text>{item.item.jobTitle}</Text>
-                            </View>
-                        </Animated.View>)
-                }
+                    <ListItem scrollY={scrollY} item={item.item} index={item.index} />
+
                 }
 
             />
         </View>
     )
+}
+
+
+
+
+
+function ListItem({ item, index, scrollY }: {
+    item: {
+        key: string;
+        image: string;
+        name: string;
+        jobTitle: string;
+        email: string;
+    }, index: number, scrollY: SharedValue<number>
+}) {
+    const scaleStyle = useAnimatedStyle(() => {
+        const inputRange = [-1, 0, ITEM_SIZE * index, ITEM_SIZE * (index + 2)];
+        const outputRange = [1, 1, 1, 0]
+        const scale = interpolate(scrollY.value, inputRange, outputRange);
+        return {
+            transform: [{ scale }]
+        }
+    })
+
+
+    const opacityStyle = useAnimatedStyle(() => {
+        const inputRange = [-1, 0, ITEM_SIZE * (index + .5), ITEM_SIZE * (index + 1)];
+        const outputRange = [1, 1, 1, 0]
+        const opacity = interpolate(scrollY.value, inputRange, outputRange);
+        return {
+            opacity
+        }
+    })
+    return (
+        <Animated.View className='flex flex-row bg-gray-200 shadow-xl shadow-black rounded-2xl' style={[{ padding: SPACING, marginBottom: SPACING }, scaleStyle, opacityStyle]}>
+            <Image src={item.image} style={{ width: AVATAR_SIZE, height: AVATAR_SIZE, borderRadius: AVATAR_SIZE, marginRight: SPACING / 2 }} />
+            <View style={{ marginLeft: SPACING }}>
+                <Text className='font-bold text-lg'>{item.name}</Text>
+                <Text className='text-blue-300'>{item.email}</Text>
+                <Text>{item.jobTitle}</Text>
+            </View>
+        </Animated.View >)
 }
